e07ee6c3ea10627047ff137d05525f69ad2c84eb
Braden McDorman <bmcdorman@gmail.com>
2012-07-19 10:49:30 -0500

    Commented out drawing code for now

--- src/draw_c.cpp
+++ src/draw_c.cpp
@@ -1,29 +1,27 @@
 #include "draw.h"
 #include "draw.hpp"
 
-Draw g_draw;
-
 int draw_open()
 {
-	return g_draw.open();
+	// return g_draw.open();
 }
 
 void draw_point(int x, int y)
 {
-	g_draw.point(x, y);
+	// g_draw.point(x, y);
 }
 
 void draw_line(int sx, int sy, int ex, int ey)
 {
-	g_draw.line(sx, sy, ex, ey);
+	// g_draw.line(sx, sy, ex, ey);
 }
 
 void draw_clear()
 {
-	g_draw.clear();
+	// g_draw.clear();
 }
 
 void draw_close()
 {
-	g_draw.close();
+	// g_draw.close();
 }
