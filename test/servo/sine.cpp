4afceaa191e53bad7132ad87835942ecc3b42158
Braden McDorman <bmcdorman@gmail.com>
2012-07-19 10:49:41 -0500

    Fixed servo sine code

--- test/servo/sine.cpp
+++ test/servo/sine.cpp
@@ -1,6 +1,7 @@
 #include <kovan.hpp>
 #include <cmath>
 #include <sys/time.h>
+#include <cstdio>
 
 #define HALF_PI 1.57
 
@@ -13,10 +14,15 @@ int main(int argc, char *argv[])
 		Servo(4)
 	};
 	
+	for(unsigned char i = 0; i < 4; ++i) servos[i].setPosition(512);
+	
 	struct timeval tv;
 	for(;;) {
 		gettimeofday(&tv, NULL);
 		float secs = (tv.tv_sec * 1000 + tv.tv_usec / 1000) / 1000.0f;
-		for(unsigned char i = 0; i < 4; ++i) servos[i].setPosition(sin(secs + HALF_PI * i) * 400 + 512);
+		printf("secs: %f", secs);
+		unsigned int ticks = sin(secs) * 400 + 512;
+		printf("\ttick: %u\n", ticks);
+		for(unsigned char i = 0; i < 4; ++i) servos[i].setPosition(ticks);
 	}
 }
