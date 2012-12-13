9da4b3c43b27f18e816b4ee1c3f79c468c02f04a
Braden McDorman <bmcdorman@gmail.com>
2012-12-13 13:26:13 -0600

    Write to i2c for accel values only once

--- src/accel.cpp
+++ src/accel.cpp
@@ -11,21 +11,21 @@ bool Acceleration::s_setup = false;
 short Acceleration::x()
 {
 	setupI2C();
-	if(!Private::I2C::instance()->write(DATA_ADDR, DATA_VAL, true)) return 0xFFFF;
+	if(s_setup) return 0xFFFF;
 	return static_cast<char>(Private::I2C::instance()->read(READ_OFFSET + 0));
 }
 
 short Acceleration::y()
 {
 	setupI2C();
-	if(!Private::I2C::instance()->write(DATA_ADDR, DATA_VAL, true)) return 0xFFFF;
+	if(s_setup) return 0xFFFF;
 	return static_cast<char>(Private::I2C::instance()->read(READ_OFFSET + 1));
 }
 
 short Acceleration::z()
 {
 	setupI2C();
-	if(!Private::I2C::instance()->write(DATA_ADDR, DATA_VAL, true)) return 0xFFFF;
+	if(s_setup) return 0xFFFF;
 	return static_cast<char>(Private::I2C::instance()->read(READ_OFFSET + 2));
 }
 
@@ -33,4 +33,5 @@ void Acceleration::setupI2C()
 {
 	if(s_setup) return;
 	s_setup = Private::I2C::instance()->pickSlave("0x1d");
+	Private::I2C::instance()->write(DATA_ADDR, DATA_VAL, true);
 }
