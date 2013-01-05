c3ad9f5ff7aac58700c9d86eaf3c5d0250eb319f
Josh S <southou@gmail>
2013-01-05 11:38:38 -0600

    adding 10-bit accelerometer sensing

--- src/accel.cpp
+++ src/accel.cpp
@@ -4,7 +4,14 @@
 #define DATA_ADDR 0x16
 #define DATA_VAL 0x05
 
-#define READ_OFFSET 6
+#define R_XOUTL = 0x0
+#define R_XOUTH = 0x1
+
+#define R_YOUTL = 0x2
+#define R_YOUTH = 0x3
+
+#define R_ZOUTL = 0x4
+#define R_ZOUTH = 0x5
 
 bool Acceleration::s_setup = false;
 
@@ -12,21 +19,29 @@ short Acceleration::x()
 {
 	setupI2C();
 	if(!s_setup) return 0xFFFF;
-	return static_cast<char>(Private::I2C::instance()->read(READ_OFFSET + 0));
+	// it is important that low is read before high
+	// reading the low 8 bits latches the high 2 bits (page 21)
+	// high bits must be read immediately afterwards
+	short x_low = static_cast<short>(Private::I2C::instance()->read(R_XOUTL));
+	short x_high = static_cast<short>(Private::I2C::instance()->read(R_XOUTH));
+	return ((x_high << 8) | x_low);
 }
 
 short Acceleration::y()
 {
 	setupI2C();
 	if(!s_setup) return 0xFFFF;
-	return static_cast<char>(Private::I2C::instance()->read(READ_OFFSET + 1));
-}
+	short y_low = static_cast<short>(Private::I2C::instance()->read(R_YOUTL));
+	short y_high = static_cast<short>(Private::I2C::instance()->read(R_YOUTH));
+	return ((y_high << 8) | y_low);}
 
 short Acceleration::z()
 {
 	setupI2C();
 	if(!s_setup) return 0xFFFF;
-	return static_cast<char>(Private::I2C::instance()->read(READ_OFFSET + 2));
+	short z_low = static_cast<short>(Private::I2C::instance()->read(R_ZOUTL));
+	short z_high = static_cast<short>(Private::I2C::instance()->read(R_ZOUTH));
+	return ((z_high << 8) | z_low);
 }
 
 void Acceleration::setupI2C()
