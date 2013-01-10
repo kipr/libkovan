4a9fe221a0acf79f46c8b448474f17562c3e6671
Braden McDorman <bmcdorman@gmail.com>
2013-01-10 02:24:05 -0600

    Added header and c++ guard to display code header

--- include/kovan/display.h
+++ include/kovan/display.h
@@ -24,5 +24,18 @@
 // NOTE: the difference between display_clear and console_clear is that display_clear also clears the display map
 // Display window size is 10 rows by 42 columns, indexed from 0
 
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void display_clear();  // clears console and sets display map to all spaces
 void display_printf(int col, int row, char *t, ...); // runs printf formatting to specified screen location
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
