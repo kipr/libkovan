c24d5cea3d81dc06937c8bc2220ce60a1016b557
Braden McDorman <bmcdorman@gmail.com>
2013-02-16 14:20:31 -0600

    Added ownership options to sensor_logic classes

--- include/kovan/sensor_logic.hpp
+++ include/kovan/sensor_logic.hpp
@@ -33,46 +33,52 @@ namespace SensorLogic
 	class EXPORT_SYM Base : public Sensor<bool>
 	{
 	public:
-		Base(const Sensor<bool> *a, const Sensor<bool> *b);
+		Base(const Sensor<bool> *a, const Sensor<bool> *b, bool owns = false);
+		~Base();
 		
 		const Sensor<bool> *a() const;
 		const Sensor<bool> *b() const;
+		bool owns() const;
 	private:
 		const Sensor<bool> *m_a;
 		const Sensor<bool> *m_b;
+		bool m_owns;
 	};
 	
 	class EXPORT_SYM And : public Base
 	{
 	public:
-		And(const Sensor<bool> *a, const Sensor<bool> *b);
+		And(const Sensor<bool> *a, const Sensor<bool> *b, bool owns = false);
 		virtual bool value() const;
 	};
 	
 	class EXPORT_SYM Or : public Base
 	{
 	public:
-		Or(const Sensor<bool> *a, const Sensor<bool> *b);
+		Or(const Sensor<bool> *a, const Sensor<bool> *b, bool owns = false);
 		virtual bool value() const;
 	};
 	
 	class EXPORT_SYM Xor : public Base
 	{
 	public:
-		Xor(const Sensor<bool> *a, const Sensor<bool> *b);
+		Xor(const Sensor<bool> *a, const Sensor<bool> *b, bool owns = false);
 		virtual bool value() const;
 	};
 	
 	class EXPORT_SYM Not : public Sensor<bool>
 	{
 	public:
-		Not(const Sensor<bool> *input);
+		Not(const Sensor<bool> *input, bool owns = false);
+		~Not();
 		virtual bool value() const;
 		
 		const Sensor<bool> *input() const;
+		bool owns() const;
 		
 	private:
 		const Sensor<bool> *m_input;
+		bool m_owns;
 	};
 }
 
