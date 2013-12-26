5570833480b0b198d8ac58388e8f998147295ef2
Stefan Zeltner <zeltner@usc.edu>
2013-12-26 18:23:40 +0100

    Update file headers

--- src/openni2_depth_driver.cpp
+++ src/openni2_depth_driver.cpp
@@ -1,23 +1,22 @@
-/* *****************************************************************************
-
-    This file is part of the depth sensor library for the
-        KIPR Link Robot Controller.
-
-    libkipr_link_depth_sensor is free software: you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    libkipr_link_depth_sensor is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with libkipr_link_depth_sensor.
-    If not, see <http://www.gnu.org/licenses/>.
-
-*******************************************************************************/
+/**************************************************************************
+ *  Copyright 2012 KISS Institute for Practical Robotics                  *
+ *                                                                        *
+ *  This file is part of libkovan.                                        *
+ *                                                                        *
+ *  libkovan is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 2 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  libkovan is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with libkovan. Check the LICENSE file in the project root.      *
+ *  If not, see <http://www.gnu.org/licenses/>.                           *
+ **************************************************************************/
 
 #include "kovan/depth_exception.hpp"
 #include "kovan/openni2_depth_driver.hpp"
