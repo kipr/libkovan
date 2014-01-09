2ebdfe1cad1fc7d073995571aae259b4ac471f08
Braden McDorman <bmcdorman@gmail.com>
2014-01-09 10:34:46 -0600

    Fixed missing define

--- include/kovan/depth.h
+++ include/kovan/depth.h
@@ -168,7 +168,7 @@ EXPORT_SYM int get_depth_value(int row, int column);
 EXPORT_SYM point3 get_world_point(int row, int column);
 
 #ifdef __linux__
-static const int INVALID_DEPTH = INT_MAX;
+static const int INVALID_DEPTH = 2147483647;
 #else
 static const int INVALID_DEPTH = INT32_MAX;
 #endif
