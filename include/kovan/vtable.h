d651f16ee9718cb8de1127a2550aa2fe0d45c2e2
Braden McDorman <bmcdorman@gmail.com>
2015-01-08 13:52:03 -0600

    fixed some anamalous behavior in C99 mode

--- include/kovan/vtable.h
+++ include/kovan/vtable.h
@@ -13,19 +13,25 @@ extern "C" {
 #define VIL
 #define VH
 
+#ifdef __GNUC__
+#define POSSIBLY_UNUSED __attribute__ ((unused))
+#else
+#define POSSIBLY_UNUSED
+#endif
+
 #define VTABLE_FUNC_VOID(name, signature, args)                       \
   typedef void (*name##_func) signature;                              \
   EXPORT_SYM extern name##_func g_##name##_func;                                 \
   EXPORT_SYM extern const name##_func g_##name##_func_default;                   \
   static const char *const name##_signature = "void" #signature;      \
-  static inline void name signature { (*g_##name##_func) args;  }
+  POSSIBLY_UNUSED static void name signature { (*g_##name##_func) args;  }
 
 #define VTABLE_FUNC(name, returnType, signature, args)                       \
   typedef returnType (*name##_func) signature;                               \
   EXPORT_SYM extern name##_func g_##name##_func;                                        \
   EXPORT_SYM extern const name##_func g_##name##_func_default;                          \
   static const char *const name##_signature = #returnType #signature;        \
-  static inline returnType name signature { return (*g_##name##_func) args;  }
+  POSSIBLY_UNUSED static returnType name signature { return (*g_##name##_func) args;  }
 
 #define VTABLE_SET_DEFAULT(name, impl)                  \
   const name##_func g_##name##_func_default = &impl;    \
